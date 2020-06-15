446a268f8a29b2cceb441aff9c2752dad245d480
Nazir-Bloch <nazirb744@gmail.com>
2020-06-15 23:58:21 +0500

    assignment javascript

---
+++ app.js
@@ -0,0 +1,167 @@
+// ==== Chapter # 1 and Task # 1
+
+// alert (<script src="app.js"></script>)
+
+// ==== Chapter # 1 and Task # 2
+
+// alert("Eror! Please enter a valid password.")
+
+// ==== Chapter # 1 and Task # 3
+
+// alert("Welcome to JS Land...\n Happy Coding!") 
+
+// ==== Chapter # 1 and Task # 4
+
+//  alert("Welcome to JS Land!");   
+
+// 		alert("Happy Coding!");  
+		
+
+// ==== Chapter # 1 and Task # 5
+
+// alert("Hello...I can run my JS through my web browser's console")
+// console.log("Hello...I can run my JS through my web broser's console")
+
+// ==== Chapter # 1 and Task # 6
+
+//  <script src="app.js"></script> add this code in html or css file 
+
+// ==== Chapter # 1 and Task # 7
+
+//  <script src="app.js"></script>
+// </head>
+// <body>
+//     <!-- <script src="app.js"></script> -->
+//    <!-- <h1>Welcome to learn JS World</h1>
+//    <script src="app.js"></script> 
+// </body>
+// </html>
+
+// ==== Chapter # 2 and Task # 1
+
+// var a = "Nazir";
+// alert(a)
+
+// ==== Chapter # 2 and Task # 2
+
+// var a = "Nazir";
+// var b = "Ahmed";
+// alert(a+b)
+
+// ==== Chapter # 2 and Task # 3(a)
+
+
+// var a = "message";
+// alert(a)
+
+// ==== Chapter # 2 and Task # 3(b)
+
+// var a = "Hello World"
+// alert(a)
+
+// ==== Chapter # 2 and Task # 3(c)
+
+// var a = "message";
+// alert(a)
+
+// ==== Chapter # 2 and Task # 4
+
+// alert("Nazir Ahmed");
+// alert("24 years old");
+// alert("certified mobile application development");
+
+
+// ==== Chapter # 2 and Task # 6
+
+// var a = "nazirb";
+// var d = 744
+// var b = "@gmail.com";
+// var c = a+d+b;
+// alert(c);
+
+
+// ==== Chapter # 2 and Task # 7
+
+// var a = "I am trying to learn from the";
+// var b = "Book";
+// var c = " A smarter way to learn JavaScript";
+// var d = a+b+c;
+// alert(d);
+
+// ==== Chapter # 2 and Task # 8
+
+// var a = "Yah! I can write HTML through JavaScript";
+// document.write(a);
+
+// ==== Chapter # 2 and Task # 9
+
+// var a = ""===========&& | &&============="";
+// document.write(a);
+
+// ==== Chapter # 3 and Task # 1
+
+// var age = "I am 15 years old";
+// alert(age);
+
+// ==== Chapter # 3 and Task # 2
+
+// skip
+
+
+// ==== Chapter # 3 and Task # 3
+
+// var birthYear = "My birth year is 1995 <br> Data type of my decleared variable is number";
+// document.write(birthYear);
+
+// ==== Chapter # 3 and Task # 4
+
+// var name = "Nazir ordered ";
+// var shirt = 5;
+// var store = " T-shirt(s) on XYZ clothing store";
+// var x = (name+shirt+store);
+// document.write(x);
+
+
+// ==== Chapter # 4 and Task # 1
+
+// var a = "Nazir";
+// var b = "Baloch";
+// var c = 1;
+// var d = a+b+c;
+// document.write(d);
+
+// ==== Chapter # 4 and Task # 2
+
+// legal = (name,$,capital name);
+// ilegall = (spacename,start with Number,not js keywords);
+
+// ==== Chapter # 4 and Task # 3(a)
+
+// name should be start with letters, dollarsign, capital letters,underscores
+
+// ==== Chapter # 4 and Task # 3(b)
+
+// letters, dollarsign,number and underscores;
+
+// ==== Chapter # 4 and Task # 3(c)
+
+// name,dollarsign,underscores,
+
+// ==== Chapter # 4 and Task # 3(d)
+
+// Sensitive
+
+// ==== Chapter # 4 and Task # 3(e)
+
+// keywords
+
+
+
+
+
+
+
+
+
+
+ 
